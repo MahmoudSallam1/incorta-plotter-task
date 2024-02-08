6493ddda01ee4afad0aefd91683061129e19465e
Mahmoud Sallam <sallam@msallam.me>
2024-02-09 01:38:55 +0200

    feat(plotter): Add app primary & delete button component

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { AppDeleteButton, AppPrimaryButton } from "./AppButton";
 
 function AppContent() {
   return (
     <div className="flex-1 bg-gray-200 p-4">
-    <h1 className="text-center pt-4">Main Content</h1>
-    <p className="text-center mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
+      <h1 className="text-center pt-4">Main Content</h1>
+      <p className="text-center mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <AppPrimaryButton text="Button" onClick={() => console.log("Hello")} />
+      <AppDeleteButton text="Delete" />
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          500: "#3600fa",
+          600: "#280377",
+        },
+      },
+    },
   },
   plugins: [],
 };
