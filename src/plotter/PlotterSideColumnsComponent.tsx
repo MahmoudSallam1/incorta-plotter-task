cc5c506771c7a604f6177480444cd3da921dcaf7
Mahmoud Sallam <sallam@msallam.me>
2024-02-09 19:10:32 +0200

    refactor(plotter): Refactor loading spinners in side columns

--- src/plotter/PlotterSideColumnsComponent.tsx
+++ src/plotter/PlotterSideColumnsComponent.tsx
@@ -21,51 +21,55 @@ function PlotterSideColumnsComponent({
   );
   return (
     <div className="bg-gray-100 w-full lg:w-1/4 h-full border-r p-4">
-      <Droppable droppableId={PlotterConst.COLUMNS}>
-        {(provided) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            className="p-4 border rounded-md"
-            style={{ minHeight: "100px" }}
-          >
-            {loading && <AppLoadingSpinner />}
-            {!loading && (
-              <aside>
-                <div>
-                  <h3 className="text-base font-medium mb-2 text-gray-600">
-                    Dimensional Columns
-                  </h3>
-                  {dimensionalColumns.map(
-                    (column: DataColumn, index: number) => (
-                      <AppDraggableCard
-                        key={column.name}
-                        column={column}
-                        index={index}
-                      />
-                    )
-                  )}
-                </div>
-                {measureColumns.length > 0 && (
-                  <div className="border-t border-gray-300 mt-4 pt-4">
+      {loading && <AppLoadingSpinner />}
+      {!loading && (
+        <Droppable droppableId={PlotterConst.COLUMNS}>
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="p-4 border rounded-md"
+              style={{ minHeight: "100px" }}
+            >
+              {!loading && (
+                <aside>
+                  <div>
                     <h3 className="text-base font-medium mb-2 text-gray-600">
-                      Measure Columns
+                      Dimensional Columns
                     </h3>
-                    {measureColumns.map((column: DataColumn, index: number) => (
-                      <AppDraggableCard
-                        key={column.name}
-                        column={column}
-                        index={index}
-                      />
-                    ))}
+                    {dimensionalColumns.map(
+                      (column: DataColumn, index: number) => (
+                        <AppDraggableCard
+                          key={column.name}
+                          column={column}
+                          index={index}
+                        />
+                      )
+                    )}
                   </div>
-                )}
-              </aside>
-            )}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
+                  {measureColumns.length > 0 && (
+                    <div className="border-t border-gray-300 mt-4 pt-4">
+                      <h3 className="text-base font-medium mb-2 text-gray-600">
+                        Measure Columns
+                      </h3>
+                      {measureColumns.map(
+                        (column: DataColumn, index: number) => (
+                          <AppDraggableCard
+                            key={column.name}
+                            column={column}
+                            index={index}
+                          />
+                        )
+                      )}
+                    </div>
+                  )}
+                </aside>
+              )}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      )}
     </div>
   );
 }
